ad2d100c8a0358ebffe80e9215d2fca949d53ede
kiranreddykasa <kiranreddy2004@gmail.com>
2018-04-17 22:46:01 +0530

    added future builder to load home page

--- lib/common/SharedPreferencesHelper.dart
+++ lib/common/SharedPreferencesHelper.dart
@@ -8,7 +8,7 @@ class SharedPreferencesHelper {
     Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
     final SharedPreferences prefs = await _prefs;
     String token = prefs.getString("token");
-    return "Bearer " + token;
+    return token != null ? "Bearer " + token : null;
   }
 
   static Future<User> getLoggedinValue() async {
